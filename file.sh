1f4061ab6e480c8e98c4e7673bbaf7cb5725fcaa
sai411 <66815779+sai411@users.noreply.github.com>
2024-10-26 21:29:51 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -6,7 +6,7 @@ echo ${CSH}-ovas-application-db-keyspace__
 
 ${DASHBOARD_ENV}-agentInfo-env="${DASHBOARD_ENV}"
 ${DASHBOARD_ENV}-account-search-url__="https://${CSH}-oneview-accountsearch.rogers.com/web/a/dashboard/account-search"
-${CSH}-ovas-application-db-keyspace__="${DASHBOARD_ENV}_ute" 
+${CSH}-ovas-application-db-keyspace__="${DASHBOARD_ENV}_ute"
 
 
-echo  ${${DASHBOARD_ENV}-agentInfo-env)
+echo "printing!!!!!!!!!!! $(${DASHBOARD_ENV}-agentInfo-env)"
